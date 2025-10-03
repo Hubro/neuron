1df7426adbc6dac082bd1e85fa52f9023329d523
Tomas Sandven <tomas@sandven.email>
2025-10-03 16:39:35 +0200

    Rename integration module to prevent confusion

--- custom_components/neuron_integration/__init__.py
+++ custom_components/neuron_integration/__init__.py
@@ -1,15 +1,22 @@
 import logging
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import Event, HomeAssistant
 
-from .const import DOMAIN  # noqa
+from neuron_integration import bus
+from neuron_integration.const import DOMAIN
+
+__all__ = ["DOMAIN", "async_setup_entry", "async_unload_entry"]
 
 LOG = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry):
     LOG.info("Setting up Neuron integration with config: %r", config.as_dict())
+
+    hass.bus.async_listen("neuron", _handle_event)
+
+    hass.bus.async_fire("neuron", {})
 
     return True
 
@@ -18,3 +25,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     LOG.info("Unloading Neuron integration entry: %r", entry.as_dict())
 
     return True
+
+
+async def _handle_event(event: Event):
+    LOG.info("Received event: %r", event.as_dict())
+    bus.parse_message(event.data)
